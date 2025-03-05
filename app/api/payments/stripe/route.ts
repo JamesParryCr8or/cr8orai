3fe7ac89ce1fd265a9c3f72b63eac8703bbf1957
JamesParryCr8or <james@cr8or.co.uk>
2025-03-05 23:43:18 +0000

    Your commit message

--- app/api/payments/stripe/route.ts
+++ app/api/payments/stripe/route.ts
@@ -9,7 +9,7 @@ import {
 
 // Initialize Stripe with the secret key from environment variables
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-01-27.acacia",
+  apiVersion: "2025-02-24.acacia",
 });
 
 // Ensure dynamic content generation for each request
